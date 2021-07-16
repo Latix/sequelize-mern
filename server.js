9aa50fe0498b71d3ccdf6902b19cb82bb5a554d7
Kamsi Kodi <kamsikodi@gmail.com>
2021-07-16 11:42:09 +0100

    Setup DB

--- server.js
+++ server.js
@@ -15,18 +15,10 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-const db = require("./models");
-// db.sequelize.sync();
-db.sequelize.sync({ force: true }).then(() => {
-    console.log("Drop and re-sync db.");
-  });
-
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
-
-require("./routes/tutorial.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
